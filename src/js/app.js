ccf1e6fb65bb0ff7cb82410d5be96d1a500cc66d
andreytver84 <uvikmedium@mail.ru>
2023-02-19 17:27:05 +0300

    try to push 2

--- src/js/app.js
+++ src/js/app.js
@@ -2,7 +2,7 @@ const collapseBtn = document.querySelector(".collapse-btn");
 const collapseBlock = document.querySelector(".collapse-block");
 const collapseContent = document.querySelector(".collapse-content");
 
-collapseBtn.addEventListener("click", (e) => {
+collapseBtn.addEventListener("click", () => {
   if (collapseBlock.classList.contains("collapsed")) {
     collapseBlock.style.maxHeight = `${collapseContent.scrollHeight + 4}px`;
     collapseBlock.classList.remove("collapsed");
