2d18334ff78ccc1728ba01e9addaaf272602deab
andreytver84 <uvikmedium@mail.ru>
2023-02-19 17:08:34 +0300

    first commit

---
+++ src/js/app.js
@@ -0,0 +1,13 @@
+const collapseBtn = document.querySelector(".collapse-btn");
+const collapseBlock = document.querySelector(".collapse-block");
+const collapseContent = document.querySelector(".collapse-content");
+
+collapseBtn.addEventListener("click", (e) => {
+  if (collapseBlock.classList.contains("collapsed")) {
+    collapseBlock.style.maxHeight = `${collapseContent.scrollHeight + 4}px`;
+    collapseBlock.classList.remove("collapsed");
+  } else {
+    collapseBlock.style.maxHeight = 0;
+    collapseBlock.classList.add("collapsed");
+  }
+});
